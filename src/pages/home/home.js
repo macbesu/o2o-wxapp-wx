d98e55ab75facb9ca78de6a4d75d5e467d0fa0a1
ivanwarke <iveon.cc@gmail.com>
2018-01-10 20:05:25 +0800

    food amount

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -11,6 +11,7 @@ Page({
     foodFilter: '*',
     foods: [],
     categories: [],
+    orders: {},
     categories_index: 0,
     currentCategory: '(全部)',
   },
@@ -62,15 +63,59 @@ Page({
       success: function(res) {
         if (res.statusCode === 200 ){
           wx.hideLoading();
-          self.setData({
-            foods: res.data,
+          const data = res.data;
+          data.forEach((item, index) => {
+            if (self.data.orders.hasOwnProperty(item._id)) {
+              item.amount = self.data.orders[item._id];
+            }
+            if (item.coupon) {
+              if (item.coupon.couponType === 0) {
+                item.newPrice = (item.price * item.coupon.value).toFixed(1);
+              } else if (item.coupon.couponType === 1 && item.coupon.limit < item.price) {
+                item.newPrice = item.price - item.coupon.value;
+              }
+              
+            }
           });
+          self.setData({ foods: data });
         }
       },
       fail: function(e) {
         wx.hideLoading();
       },
     });
+  },
+  buyAddOne: function(e) {
+    const { id, index } = e.target.dataset;
+    const orders = Object.assign({}, this.data.orders);
+    const foods = this.data.foods;
+    if (orders.hasOwnProperty(id)) {
+      if (orders[id] < 20) {
+        orders[id] += 1;
+      }
+    } else {
+      orders[id] = 1;
+    }
+    foods.forEach((item, index) => {
+      if (orders.hasOwnProperty(item._id)) {
+        item.amount = orders[item._id];
+      }
+    });
+    this.setData({ orders: orders, foods: foods });
+  },
+  buyRemoveOne: function(e) {
+    const { id, index } = e.target.dataset;
+    const orders = Object.assign({}, this.data.orders);
+    const foods = this.data.foods;
+    if (orders[id] > 0) {
+      orders[id] -= 1;
+    }
+    foods.forEach((item, index) => {
+      if (orders.hasOwnProperty(item._id)) {
+        item.amount = orders[item._id];
+      }
+    });
+    this.setData({ orders: orders, foods: foods });
   },
   showInput: function () {
     this.setData({
